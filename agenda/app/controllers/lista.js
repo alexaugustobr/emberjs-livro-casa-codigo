21154763c974881e8519936174253e53d1d35b36
alexcologia33 <alexcologia33@gmail.com>
2017-12-02 20:36:00 -0200

    inicio

---
+++ agenda/app/controllers/lista.js
@@ -0,0 +1,52 @@
+import Controller from '@ember/controller';
+
+
+let Pessoa = Ember.Object.extend({
+    nome: 'Clairton' ,
+    sobrenome: 'Heinzen',
+    nomeCompleto: Ember.computed('nome', 'sobrenome', function(){
+        return `${this.get('nome')} ${this.get('sobrenome')}`;
+    }),
+    aoAlterarNome : Ember.observer('nomeCompleto', function(){
+        console.log(`nome alterado para ${this.get('nomeCompleto')}`);
+    }),
+    telefones: [],
+    prefixos: Ember.computed('telefones.@each.prefixo', function(){
+        return this.get('telefones').mapBy('prefixo');
+    })
+});
+
+
+let pessoa = Pessoa.create({
+      nome: 'João',
+      sobrenome: 'Silva'
+    });
+
+let residencial = Ember.Object.create({
+    prefixo: 49,
+    numero: '33333333'
+});
+
+pessoa.get('telefones').pushObject(residencial);
+
+let comercial = Ember.Object.create({
+    prefixo: 47,
+    valor: '99999999'
+});
+
+pessoa.get('telefones').pushObject(comercial);
+
+console.log(pessoa.get('nomeCompleto'));
+
+console.log(pessoa.set('nomeCompleto', 'Daniel <3'));
+
+console.log(pessoa.get('telefones'));
+
+console.log(pessoa.get('prefixos'));
+
+export default Controller.extend({
+    nome: pessoa.get('nomeCompleto')
+
+
+    
+});
